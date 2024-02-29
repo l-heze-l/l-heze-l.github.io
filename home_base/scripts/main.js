9fb78d5e978faea1d1a5a876461949e3c942e4da
l-heze-l <luke.hezel@gmail.com>
2024-02-29 16:10:55 -0600

    version 2.03: added project descriptions

--- home_base/scripts/main.js
+++ home_base/scripts/main.js
@@ -1,96 +1,23 @@
 /////////////////////////////////////////////////////////////////////
-// Lukas Hezel
-// 10/07/23
-// Homebase JS Code 
+//  Lukas Hezel
+//  02/27/24
+//  Home-Base JS Code 
 /////////////////////////////////////////////////////////////////////
-// Declarations:
 
-const myImage = document.querySelector("img");
-let myButton = document.querySelector("button");
-const tabButtons = document.querySelectorAll(".tab-button");
-const tabContents = document.querySelectorAll(".tab-content");
-const openModalButton = document.getElementById("open-modal");
-const closeModalButton = document.getElementById("close-modal");
-const projectModal = document.getElementById("project-modal");
-const projectNameInput = document.getElementById("project-name");
-const addProjectButton = document.getElementById("add-project");
-const projectList = document.getElementById("tab-container");
+var sidebar = document.getElementById('sidebar');
 
-/////////////////////////////////////////////////////////////////////
-// Boar Button:
-
-myButton.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/exploding-boar-boarsplosion.png") {
-    myImage.setAttribute("src", "images/boar-wink.png");
-  } else {
-    myImage.setAttribute("src", "images/exploding-boar-boarsplosion.png");
-  }
-};
-
-/////////////////////////////////////////////////////////////////////
-// Collapsing Tabs:
-
-tabButtons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    tabContents.forEach((content) => {
-      content.style.display = "none";
-    });
-    tabContents[index].style.display = "block";
-  });
-});
-tabContents[0].style.display = "block";
-
-/////////////////////////////////////////////////////////////////////
-// New Project Button:
-
-function openModal() {
-  projectModal.style.display = "block";
-}
-
-function closeModal() {
-  projectModal.style.display = "none";
-  projectNameInput.value = ""; 
-}
-
-openModalButton.addEventListener("click", openModal);
-closeModalButton.addEventListener("click", closeModal);
-
-projectModal.addEventListener("click", (event) => {
-  if (event.target === projectModal) {
-    closeModal();
-  }
-});
-
-projectNameInput.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    addProject();
-    closeModal(); 
-  }
-});
-
-/////////////////////////////////////////////////////////////////////
-// Add New Project Function:
-
-function addProject() {
-  const projectName = projectNameInput.value.trim();
-  const projectDesc = projectDescInput.value.trim();
-
-  if (projectName !== "") {
-    const listItem = document.createElement("li");
-    listItem.textContent = projectName;
-    projectList.appendChild(listItem);
-
-    projectNameInput.value = "";
+function showPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  if (popup) {
+      popup.style.display = "block";
+      sidebar.style.display = "none";
   }
 }
 
-addProjectButton.addEventListener("click", addProject);
-
-projectNameInput.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    addProject();
+function hidePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  if (popup) {
+      popup.style.display = "none";
+      sidebar.style.display = "block";
   }
-});
-
-/////////////////////////////////////////////////////////////////////
+}
